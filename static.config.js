f3a4e4830deb5a929be1cceea85a53bccce56db4
Mark Hatch <mark-hatch@pluralsight.com>
2021-11-05 14:13:43 -0600

    fix static.config.js

--- static.config.js
+++ static.config.js
@@ -3,9 +3,12 @@ import path from 'path'
 export default {
   plugins: [
     [
+      require.resolve('react-static-plugin-source-filesystem'),
       {
         location: path.resolve('./src/pages'),
       },
     ],
+    require.resolve('react-static-plugin-reach-router'),
+    require.resolve('react-static-plugin-sitemap'),
   ],
 }
